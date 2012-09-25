78576f5c41455d3ece8a2bafceaf54598e8615fc
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-25 17:36:21 +0530

    signup.php changed

--- signup.php
+++ signup.php
@@ -1,3 +1,30 @@
+<?php
+include("cse.php");
+?>
 <html>
+<head>
+</head>
+<body>
+<form name="signup" action="save_signup.php">
+<table>
+  <tr>
+    <td>NAME</td>
+    <td><input type="text" name="name" /></td>
+  </tr>
+  <tr>
+    <td>IITK EMAIL</td>
+    <td><input type="text" name="email" /></td>
+  </tr>
+  <tr>
+    <td>CATEGORY</td>
+    <td>
+	<select name="category">
+	<option> </option>
+	</select>
+	</td>
+  </tr>
+</table>
 
+</form>
+</body>
 </html>
