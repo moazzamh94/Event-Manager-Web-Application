fbe739aa89aa87a986696437f1e247bf044cfd89
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-25 19:02:40 +0530

    congrat.gif welcome image added and save_signup.php changed

--- save_signup.php
+++ save_signup.php
@@ -1,6 +1,3 @@
-<?php
-include("cse.php");
-?>
 <!--username passoword confirmpassword name email category -->
 <?php
 $flag=1;
@@ -9,6 +6,43 @@ else if($_REQUEST['passoword']=='') {$_SESSION['error']="passoword is missing";$
 else if($_REQUEST['confirmpassword']=='') {$_SESSION['error']="please confirm your passowrd";$flag=0;}
 else if($_REQUEST['name']=='') {$_SESSION['error']="name is missing";$flag=0;}
 else if($_REQUEST['email']=='') {$_SESSION['error']="email is missing";$flag=0;}
-else if($_REQUEST['categor']=='-1') {$_SESSION['error']="please select your category";$flag=0;}
+else if($_REQUEST['category']=='-1') {$_SESSION['error']="please select your category";$flag=0;}
+else if($_REQUEST['passoword']!=$_REQUEST['confirmpassword']) {$_SESSION['error']="passwords do not match"; $flag=0;}
 if($flag==0) {header('Location:signup.php');}
 ?>
+<html>
+<head>
+<style type="text/css">
+#message
+{
+color:#00FF00;
+background-color:#FFFF00;
+}
+</style>
+<script language="javascript">
+function gotohome()
+{
+window.open("home.php");
+}
+setTimeout(gotohome,7000);
+</script>
+</head>
+<body background="congrat.gif">
+<center>
+<div id="message">
+<h2>
+!!!CONGRATULATIONS YOU HAVE REGISTERED SUCCESSFULLY
+<br />
+YOUR ACCOUNT DETAILS HAVE BEEN MAILED AT YOU E-MAIL ID
+<br />
+NOW YOU CAN LOGIN TO YOUR ACCOUNT
+<br />
+</h2>
+<h1>
+<a href="home.php">GO TO HOME</a>
+</h1>
+</h2>
+</div>
+</center>
+<br />
+</body>
