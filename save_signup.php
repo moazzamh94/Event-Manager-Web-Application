c0dc6991518fb8fa9c2b3d7e24946e2ff91faf40
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-26 12:57:32 +0530

    save_signup.php file changed

--- save_signup.php
+++ save_signup.php
@@ -1,18 +1,3 @@
-<!--username passoword confirmpassword name email category -->
-<?php
-include("connect.php");
-session_start();
-$flag=1;
-$user=$_REQUEST['username'];
-if($user=='') {$_SESSION['error']="user name is missing";$flag=0;}
-else {$pass=$_REQUEST['password'];if($pass=='') {$_SESSION['error']="passoword is missing";$flag=0;}
-else if($_REQUEST['confirmpassword']=='') {$_SESSION['error']="please confirm your passowrd";$flag=0;}
-else {$name=$_REQUEST['name'];if($name=='') {$_SESSION['error']="name is missing";$flag=0;}
-else {$email=$_REQUEST['email'];if($email=='') {$_SESSION['error']="email is missing";$flag=0;}
-else {$category=$_REQUEST['category'];if($_REQUEST['category']=='-1') {$_SESSION['error']="please select your category";$flag=0;}
-else if($_REQUEST['password']!=$_REQUEST['confirmpassword']) {$_SESSION['error']="passwords do not match"; $flag=0;}
-if($flag==0) {header('Location:signup.php');}}}}}
-?>
 <html>
 <head>
 <style type="text/css">
@@ -27,10 +12,17 @@ function gotohome()
 {
 window.open("home.php");
 }
-//setTimeout(gotohome,7000);
+setTimeout(gotohome,7000);
 </script>
 </head>
 <body background="congrat.gif">
+<?php
+$name=$_SESSION['name'];
+$user=$_SESSION['user'];
+$pass=$_SESSION['pass'];
+$email=$_SESSION['email'];
+$category=$_SESSION['category'];
+?>
 <center>
 <div id="message">
 <h2>
