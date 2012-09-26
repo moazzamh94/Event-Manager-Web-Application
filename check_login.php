db2b885d69a9e32277c5cb3e8a36b45e57d0ece1
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-26 16:19:40 +0530

    check_login.php slightly modified

--- check_login.php
+++ check_login.php
@@ -1,4 +1,16 @@
 <?php
 include("connect.php");
+if($_REQUEST['user']=='') {$flag=0;$_SESSION['error']='NAME MISSING';}
+else if ($_REQUEST['pass']=='') {$flag=0;$_SESSION['error']='PASSWORD MISSING';}
+if($flag==0) header('Location:login.php');
 $user=$_REQUEST['user'];
+$pass=$_REQUEST['pass'];
+$q=mysql_query("select * from `member`");
+$flag=0;
+while($a=mysql_fetch_array($q))
+{
+echo $a['user']."        ".$user."           ".$a['pass']."            ".$a['pass']."<br>";
+if(($a['user']==$user) and ($a['pass']==$pass)) {$flag=1;break;}
+}
+//if($flag==0) header('Location:login.php');
 ?>
