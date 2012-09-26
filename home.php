d46b574ed014a1fea1045ccae3910cbe68768d2a
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-26 19:15:35 +0530

    a prototype of today's events

--- home.php
+++ home.php
@@ -10,4 +10,26 @@ include("cse.php");
 </a>
 </image>
 </body>
+<center>
+<h3>
+TODAY'S EVENTS
+</h3>
+<table border="10px">
+<tr bgcolor="#00FF00">
+<th>EVENT</th><th>ROOM NO.</th><th>BY</th><th>SCHEDULE</th><th>EVENT TYPE</th>
+</tr>
+<tr>
+<td>DATA STRUCTURE</td><td>101</td><td>SURENDRA BASWANA</td><td>8:00-10:00</td><td>NORMAL CLASS</td>
+</tr>
+<tr>
+<td>HACK YOU</td><td>102</td><td>ST. RATHORE</td><td>9:00-11:00</td><td>SEMINAR</td>
+</tr>
+<tr>
+<td>COMPILERS</td><td>102</td><td>ARNAB BHATTACHARYA</td><td>12:00-15:00</td><td>EXTRA CLASS</td>
+</tr>
+<tr>
+<td>AUTOMATA</td><td>210</td><td>DHEERAJ SANGHI</td><td>10:00-15:00</td><td>EXTRA CLASS</td>
+</tr>
+</table>
+</center>
 </html>
