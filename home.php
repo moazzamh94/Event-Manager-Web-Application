50a5701294f50f4eef65dd33f86e4112eabd545d
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-26 19:25:32 +0530

    home.php furthur modifications

--- home.php
+++ home.php
@@ -14,7 +14,10 @@ include("cse.php");
 <h3>
 TODAY'S EVENTS
 </h3>
-<table border="10px">
+<h4>
+ROOM NO.101
+</h4>
+<table border="10px" width="1000px">
 <tr bgcolor="#00FF00">
 <th>EVENT</th><th>ROOM NO.</th><th>BY</th><th>SCHEDULE</th><th>EVENT TYPE</th>
 </tr>
@@ -22,14 +25,30 @@ TODAY'S EVENTS
 <td>DATA STRUCTURE</td><td>101</td><td>SURENDRA BASWANA</td><td>8:00-10:00</td><td>NORMAL CLASS</td>
 </tr>
 <tr>
-<td>HACK YOU</td><td>102</td><td>ST. RATHORE</td><td>9:00-11:00</td><td>SEMINAR</td>
+<td>HACK YOU</td><td>101</td><td>ST. RATHORE</td><td>11:00-13:00</td><td>SEMINAR</td>
 </tr>
 <tr>
-<td>COMPILERS</td><td>102</td><td>ARNAB BHATTACHARYA</td><td>12:00-15:00</td><td>EXTRA CLASS</td>
+<td>COMPILERS</td><td>101</td><td>ARNAB BHATTACHARYA</td><td>14:00-16:00</td><td>EXTRA CLASS</td>
 </tr>
 <tr>
-<td>AUTOMATA</td><td>210</td><td>DHEERAJ SANGHI</td><td>10:00-15:00</td><td>EXTRA CLASS</td>
+<td>AUTOMATA</td><td>101</td><td>DHEERAJ SANGHI</td><td>17:00-19:00</td><td>EXTRA CLASS</td>
 </tr>
 </table>
+</center>
+<center>
+ROOM NO.102
+<table border="10px" width="1000px">
+<tr bgcolor="#00FF00">
+<th>EVENT</th><th>ROOM NO.</th><th>BY</th><th>SCHEDULE</th><th>EVENT TYPE</th>
+</tr>
+<tr>
+<td>ACA MEETING</td><td>102</td><td>SHIKHER</td><td>8:00-10:00</td><td>SEMINAR</td>
+</tr>
+<tr>
+<td>ALGORITHMS</td><td>102</td><td>SURENDRA BASWANA</td><td>11:00-14:00</td><td>NORMAL CLASS</td>
+</tr>
+<tr>
+<td>HACKING101</td><td>102</td><td>DHEERAJ SANGHI</td><td>15:00-17:00</td><td>EXTRA CLASS</td>
+</tr>
 </center>
 </html>
