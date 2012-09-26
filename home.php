42a02755bfc508ad0a61d2c8c8e021f6316e11ca
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-26 18:35:19 +0530

    furthur modofication in home.php

--- home.php
+++ home.php
@@ -3,17 +3,10 @@ include("cse.php");
 ?>
 <html>
 <head>
-<style type="text/css">
-#logo
-{
-position:relative;
-azimuth:right;
-}
-</style>
 </head>
 <body>
 <a href="http://www.cse.iitk.ac.in">
-<img src="cse.gif" id="logo"  align="right"/>
+<img src="cse.gif"  align="right"/>
 </a>
 </image>
 </body>
