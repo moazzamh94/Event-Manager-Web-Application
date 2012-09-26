1db2c5af631412a1b4d8081775c5c6c1f0b296f9
MOAZZAM HUSSAIN <mhussain@iitk.ac.in>
2012-09-26 18:31:01 +0530

    cse logo included in home.php

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+include("cse.php");
+?>
+<html>
+<head>
+<style type="text/css">
+#logo
+{
+position:relative;
+azimuth:right;
+}
+</style>
+</head>
+<body>
+<img src="cse.gif" id="logo"  align="right"/>
+</image>
+</body>
+</html>
